240fd59a2bba3732ddf73fa67e8f29edc3135ac2
beamind <liyd06@qq.com>
2019-08-06 23:25:47 +0800

    add comment

--- linear_model/LinearRegression.py
+++ linear_model/LinearRegression.py
@@ -2,8 +2,27 @@ import math
 import numpy as np
 
 
-class LinearRegression():
-    def __init__(self, learning_rate=0.001, batch_size=10, train_step=10000, alpha=0.0, rand_var=0.0001, rand_seed=0,
+class LinearRegression(object):
+    """Linear least square with L2 regularization, optimized with SGD.
+
+    Args:
+    ------
+    learning_rate: float, learning rate.
+    batch_size: int, batch size.
+    train_step: int, number of training steps.
+    alpha: float, weight decay coefficient for L2 regularization. Must be positive or zero.
+    rand_var: float, variance of normal distribution for weight initialization.
+    rand_seed: int, random seed for random initialization.
+    silent: boolean, whether to print training information in training process.
+    """
+
+    def __init__(self,
+                 learning_rate=0.001,
+                 batch_size=10,
+                 train_step=10000,
+                 alpha=0.0,
+                 rand_var=0.01,
+                 rand_seed=0,
                  silent=False):
         self.lr = learning_rate
         self.bsz = batch_size
@@ -16,6 +35,14 @@ class LinearRegression():
         self.loss = 0
 
     def fit(self, x, y):
+        """
+        Args:
+        ------
+        x: features of training data, assuming the first column
+           is all 1. shape=[n_examples, n_features].
+        y: targets of training data. shape=[n_examples].
+        """
+
         n, m = x.shape
         np.random.seed(self.rand_seed)
         self.weight = np.random.randn(m) * math.sqrt(self.rand_var)
@@ -27,16 +54,19 @@ class LinearRegression():
             if not self.silent and i % 100 == 0:
                 print("step {}: ,loss: {:.5f}".format(i, self.loss))
 
-    def _get_gradients(self, X, y):
-        y_pred = np.dot(X, self.weight)
+    def _get_gradients(self, x, y):
+        """gradients calculation using batch examples."""
+        y_pred = np.dot(x, self.weight)
         error = np.expand_dims(y_pred - y, axis=1)
         loss = np.square(error).mean()
         if self.alpha > 0.0:
             loss += self.alpha * np.square(self.weight[1:]).sum()
-        gradients = (error * X).mean(axis=0)
+        gradients = (error * x).mean(axis=0)
         if self.alpha > 0.0:
+            # L2 regularization don't penalize bias.
             gradients += self.alpha * np.concatenate([[0], self.weight[1:]], axis=0)
         return gradients, loss
 
     def _apply_gradients(self, g):
+        """update weight using gradients."""
         self.weight -= self.lr * g
