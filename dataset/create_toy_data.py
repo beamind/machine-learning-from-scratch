1cd87a900e3646fda7e57674d9ac52c1aeba3f04
beamind <liyd06@qq.com>
2019-08-11 15:52:53 +0800

    add perceptron

--- dataset/create_toy_data.py
+++ dataset/create_toy_data.py
@@ -18,3 +18,14 @@ def toy_data_for_logistic_regression(weight, bias, n, variance=0.1, seed=0):
     logit = np.dot(x, weight) + bias + np.random.randn(n) * math.sqrt(variance)
     y = np.where(logit > 0, 1, 0)
     return x, y
+
+
+def toy_data_for_perceptron(weight, bias, n, variance=0.1, seed=0):
+    np.random.seed(seed)
+    x = np.random.randn(n, len(weight))
+    offset = np.zeros(len(weight))
+    offset[0] = -bias / weight[0]
+    x += offset
+    logit = np.dot(x, weight) + bias + np.random.randn(n) * math.sqrt(variance)
+    y = np.where(logit > 0, 1, -1)
+    return x, y
